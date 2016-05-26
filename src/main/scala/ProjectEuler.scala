322ae77b5f06c74926e1236800df8c24557a48e4
Mihai Safta <saftacatalinmihai@gmail.com>
2016-05-26 19:56:38 +0300

    trying to generate primes with Streams, Stack Overflow ... Will need to try tail recursive sieve

--- src/main/scala/ProjectEuler.scala
+++ src/main/scala/ProjectEuler.scala
@@ -29,6 +29,16 @@ object ProjectEuler {
       _nextPrime(primes.head + 1)
     }
   }
+
+  def getPrimes(until: Long) = {
+    def sieve(s: Stream[Int]): Stream[Int] =
+      s.head #:: sieve(s.tail filter (_ % s.head != 0))
+
+    val primes = sieve(Stream.from(2))
+
+    primes.takeWhile(_ < until).toList
+  }
+
   def projectEuler3 = {
 
     def divide(n: Long, p: Long): Long =
@@ -79,7 +89,6 @@ object ProjectEuler {
   }
 
   def projectEuler7 = {
-
     (1 to 10000).foldLeft(List(2L)){
       (primeList, idx) => nextPrime(primeList) :: primeList
     }.head
@@ -87,9 +96,24 @@ object ProjectEuler {
 
   def projectEuler8 = {
     val longNum = "73167176531330624919225119674426574742355349194934\n96983520312774506326239578318016984801869478851843\n85861560789112949495459501737958331952853208805511\n12540698747158523863050715693290963295227443043557\n66896648950445244523161731856403098711121722383113\n62229893423380308135336276614282806444486645238749\n30358907296290491560440772390713810515859307960866\n70172427121883998797908792274921901699720888093776\n65727333001053367881220235421809751254540594752243\n52584907711670556013604839586446706324415722155397\n53697817977846174064955149290862569321978468622482\n83972241375657056057490261407972968652414535100474\n82166370484403199890008895243450658541227588666881\n16427171479924442928230863465674813919123162824586\n17866458359124566529476545682848912883142607690042\n24219022671055626321111109370544217506941658960408\n07198403850962455444362981230987879927244284909188\n84580156166097919133875499200524063689912560717606\n05886116467109405077541002256983155200055935729725\n71636269561882670428252483600823257530420752963450"
-    val chars = longNum.replace("\n","").split("""|\n""").map(_.toInt)
+    val chars = longNum.replace("\n","").split("""|\n""").map(_.toLong)
     val slices = chars.sliding(13)
     slices.map(a => a.product ).toList.max
+  }
+
+  def projectEuler9 = {
+    val m = 1000
+    val found = for {
+      a <- 0 to m
+      b <- a + 1 to m
+      c <- b + 1 to m
+      if ((a * a) + (b * b)) == c * c && a + b + c == 1000
+    } yield (a, b, c)
+    found.head._1 * found.head._2 * found.head._3
+  }
+
+  def projectEuler10 = {
+    getPrimes(2000000).sum
   }
 
   def main(args: Array[String]) {
@@ -100,8 +124,11 @@ object ProjectEuler {
 //    assert(projectEuler5 == 232792560)
 //    assert(projectEuler6 == 25164150)
 //    assert(projectEuler7 == 104743)
+//    assert(projectEuler8 == 23514624000L)
+//    assert(projectEuler9 == 31875000)
 
-    println(projectEuler8)
+    println(projectEuler10)
+
 
   }
 }
--- src/main/scala/sandbox.sc
+++ src/main/scala/sandbox.sc
@@ -1,3 +1,6 @@
-val chars = "23857\n495".replace("\n","").split("""|\n""")
-val slices = chars.sliding(6)
-for (s <- slices) println(s(0))
+def sieve(s: Stream[Int]): Stream[Int] =
+  s.head #:: sieve(s.tail filter (_ % s.head != 0))
+
+val primes = sieve(Stream.from(2))                     //> primes  : Stream[Int] = Stream(2, ?)
+
+primes.take(10).toList                          //> res1: List[Int] = List(2, 3, 5, 7, 11, 13, 17, 19, 23, 29)
