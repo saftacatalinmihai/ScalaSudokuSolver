1e68144a9ec29cda19c9a4a12069e2a9f4fff260
casafta <catalin.safta@endava.com>
2016-05-27 12:33:47 +0300

    Euler 10, 11

--- src/main/scala/ProjectEuler.scala
+++ src/main/scala/ProjectEuler.scala
@@ -19,10 +19,10 @@ object ProjectEuler {
     fibSeq(4000000 - 1).filter(_ % 2 == 0).sum
   }
 
-  def nextPrime(primes: List[Long] = List()): Long = {
+  def nextPrimeSieve(primes: List[Int]): Int = {
     if (primes.isEmpty) 2
     else {
-      def _nextPrime(candidate: Long): Long = {
+      def _nextPrime(candidate: Int): Int = {
         if (primes.forall(candidate % _ != 0)) candidate
         else _nextPrime(candidate + 1)
       }
@@ -30,26 +30,28 @@ object ProjectEuler {
     }
   }
 
-  def getPrimes(until: Long) = {
-    def sieve(s: Stream[Int]): Stream[Int] =
-      s.head #:: sieve(s.tail filter (_ % s.head != 0))
+  def getPrimes(until: Int) = {
 
-    val primes = sieve(Stream.from(2))
+    lazy val primes: Stream[Int] = 2 #:: primes.map(a => {
+        Stream.from(a + 1)
+          .find(b => primes.takeWhile(c => c * c <= b).forall(b % _ != 0)).get
+      }
+    )
 
-    primes.takeWhile(_ < until).toList
+    primes.takeWhile(_ < until)
   }
 
   def projectEuler3 = {
 
-    def divide(n: Long, p: Long): Long =
+    def divide(n: Long, p: Int): Long =
       if (n % p != 0) n
       else divide(n / p, p)
 
-    def largestPrimeFactor(n: Long): Long = {
-      def iterate(n: Long, primes: List[Long]): Long = {
+    def largestPrimeFactor(n: Long): Int = {
+      def iterate(n: Long, primes: List[Int]): Int = {
          if (n <= primes.head) primes.head
-         else if (n % primes.head == 0) iterate(divide(n, primes.head),  nextPrime(primes) :: primes)
-         else iterate(n, nextPrime(primes) :: primes)
+         else if (n % primes.head == 0) iterate(divide(n, primes.head),  nextPrimeSieve(primes) :: primes)
+         else iterate(n, nextPrimeSieve(primes) :: primes)
       }
 
       iterate(n, List(2))
@@ -89,8 +91,8 @@ object ProjectEuler {
   }
 
   def projectEuler7 = {
-    (1 to 10000).foldLeft(List(2L)){
-      (primeList, idx) => nextPrime(primeList) :: primeList
+    (1 to 10000).foldLeft(List(2)){
+      (primeList, idx) => nextPrimeSieve(primeList) :: primeList
     }.head
   }
 
@@ -113,7 +115,63 @@ object ProjectEuler {
   }
 
   def projectEuler10 = {
-    getPrimes(2000000).sum
+    getPrimes(2000000).foldLeft(0L)(_+_)
+  }
+
+  def projectEuler11 = {
+
+    val grid =
+      """08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+        |49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
+        |81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
+        |52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
+        |22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
+        |24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
+        |32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
+        |67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
+        |24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
+        |21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
+        |78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
+        |16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
+        |86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
+        |19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
+        |04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
+        |88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
+        |04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
+        |20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
+        |20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
+        |01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48""".stripMargin
+
+    val intGrid = grid.split("\\r?\\n").map( l => l.split(" ").map(_.toInt).toList).toList
+    val rowLen = intGrid.head.length
+    val rowNr = intGrid.length
+
+    val prodsDiagRight = for {
+      row <- 0 to rowNr - 4
+      col <- 0 to rowLen - 4
+      (n1, n2, n3, n4) = ( intGrid(row)(col), intGrid(row + 1)(col + 1), intGrid(row + 2)(col + 2), intGrid(row + 3)(col + 3))
+    } yield List(n1, n2, n3, n4).product
+
+    val prodsDiagLeft = for {
+      row <- 0 to rowNr - 4
+      col <- 0 to rowLen - 4
+      (n1, n2, n3, n4) = ( intGrid(row)(col + 3), intGrid(row + 1)(col + 2), intGrid(row + 2)(col + 1), intGrid(row + 3)(col))
+    } yield List(n1, n2, n3, n4).product
+
+    val prodsLine = for {
+      row <- 0 until rowNr
+      col <- 0 to rowLen - 4
+      (n1, n2, n3, n4) = ( intGrid(row)(col + 3), intGrid(row)(col + 2), intGrid(row)(col + 1), intGrid(row)(col))
+    } yield List(n1, n2, n3, n4).product
+
+    val prodsCols = for {
+      row <- 0 to rowNr - 4
+      col <- 0 until rowLen
+      (n1, n2, n3, n4) = ( intGrid(row)(col), intGrid(row +1 )(col), intGrid(row + 2)(col), intGrid(row + 3)(col))
+    } yield List(n1, n2, n3, n4).product
+
+    List(prodsDiagRight.max, prodsDiagLeft.max, prodsLine.max, prodsCols.max)
+      .max
   }
 
   def main(args: Array[String]) {
@@ -126,9 +184,8 @@ object ProjectEuler {
 //    assert(projectEuler7 == 104743)
 //    assert(projectEuler8 == 23514624000L)
 //    assert(projectEuler9 == 31875000)
-
-    println(projectEuler10)
-
+//    assert(projectEuler10 == 142913828922L)
+//    assert(projectEuler11 == 70600674)
 
   }
 }
--- src/main/scala/sandbox.sc
+++ src/main/scala/sandbox.sc
@@ -1,6 +1,9 @@
-def sieve(s: Stream[Int]): Stream[Int] =
-  s.head #:: sieve(s.tail filter (_ % s.head != 0))
+lazy val ps: Stream[Int] = 2 #:: ps.map(
+  i => Stream.from(i + 1)
+    .find(
+      j => ps.takeWhile(k => k * k <= j).forall(j % _ > 0)
+    )
+    .get)
 
-val primes = sieve(Stream.from(2))                     //> primes  : Stream[Int] = Stream(2, ?)
-
-primes.take(10).toList                          //> res1: List[Int] = List(2, 3, 5, 7, 11, 13, 17, 19, 23, 29)
+val r = ps.view.takeWhile(_ < 2000000).foldLeft(0L)(_ + _)
+//val r = ps.takeWhile(_ < 2000000).foldLeft(0L)(_ + _)
